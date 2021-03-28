5f00f269a5e37ba8629e43718bb060a220572523
Purpl3Hattt <66141483+PurpleHattt@users.noreply.github.com>
2021-03-28 22:44:14 +0300

    Create after-shutdown.py

---
+++ after-shutdown.py
@@ -0,0 +1,24 @@
+import os 
+import sys 
+import apt
+import time
+print(Fore.YELLOW)
+print('-' * 50) 
+print(Fore.RESET)
+print("insert your wifi adapter(s)")
+print(Fore.YELLOW)
+print('-' * 50)
+print(Fore.RESET)
+os.system("ip a")
+time.sleep(20)
+os.system("sudo apt install -y python3-setuptools libnl-3-dev libnl-genl-3-dev libssl-dev")
+os.system("sudo apt update")
+print(Fore.YELLOW)
+print('-' * 50)
+print(Fore.CYAN)
+print('The script is done, Happy hacking!')
+print('𝔐𝔞𝔡𝔢 𝔟𝔶 𝔓𝔲𝔯𝔭𝔩3ℌ𝔞𝔱𝔱𝔱 - 𝔊𝔦𝔱𝔥𝔲𝔟')
+
+
+
+
